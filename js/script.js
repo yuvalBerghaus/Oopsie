df3deafb3ac392482a6d057fd09166aa448c2723
yuvalBerghaus <markberghaus@outlook.com>
2021-06-03 23:02:39 +0300

    form page was added

--- js/script.js
+++ js/script.js
@@ -7,18 +7,33 @@ window.onload = function () {
         let selectedCategory = document.getElementById("selectedCategory").value;
         // document.getElementById("userList").innerHTML += `Permission: ${selectedPermission}<br>Category: ${selectedCategory}<br>Username: ${userName}`
         document.getElementById("addedUsers").innerHTML += `
-        <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-        <div class="card-header">${userName} was added to the list!</div>
-        <div class="card-body">
-            <p class="card-text">Permission: ${selectedPermission}</p>
-            <p class="card-text">Category: ${selectedCategory}</p>
-        </div>
-    </div>
+    <tr>
+        <td>${userName}</td>
+        <td>${selectedPermission}</td>
+        <td>${selectedCategory}</td>
+    </tr>
         `
-    });
-    document.getElementById("clearAddUser").addEventListener("click", function () {
-        console.log("fdsfsdf");
-        document.getElementById("username") = "";
-    });
+    }
+    );
+    document.getElementById("addCarButton").addEventListener("click", function () {
+        let carModel = document.getElementById("carModel").value;
+        let plateNum = document.getElementById("plateNum").value;
+        // document.getElementById("userList").innerHTML += `Permission: ${selectedPermission}<br>Category: ${selectedCategory}<br>Username: ${userName}`
+        document.getElementById("addedCars").innerHTML += `
+    <tr>
+        <td>${carModel}</td>
+        <td>${plateNum}</td>
+    </tr>
+        `
+    }
+    );
 
 }
+
+/* <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+<div class="card-header">${userName} was added to the list!</div>
+<div class="card-body">
+    <p class="card-text">Permission: ${selectedPermission}</p>
+    <p class="card-text">Category: ${selectedCategory}</p>
+</div>
+</div> */
